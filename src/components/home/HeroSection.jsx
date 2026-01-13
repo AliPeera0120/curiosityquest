baf11e437ddfada3016eecd3bb6655b64dfc3184
AJ Peera <peeraali01@gmail.com>
2026-01-13 00:52:55 +0000

    File changes

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { createPageUrl } from '../../utils';
+import { createPageUrl } from '@/utils';
 import { Button } from '@/components/ui/button';
 import { Rocket, ArrowRight, Beaker, Lightbulb } from 'lucide-react';
 import { motion } from 'framer-motion';
