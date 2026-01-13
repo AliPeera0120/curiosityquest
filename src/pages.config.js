31c1a4b81244fae532078128064c39ea38f3d99e
Alijawad Peera <curiosity.quest25@gmail.com>
2026-01-13 00:17:07 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,6 +1,7 @@
 import Home from './pages/Home';
 import Activities from './pages/Activities';
 import Events from './pages/Events';
+import ThisWeekInSTEM from './pages/ThisWeekInSTEM';
 import __Layout from './Layout.jsx';
 
 
@@ -8,6 +9,7 @@ export const PAGES = {
     "Home": Home,
     "Activities": Activities,
     "Events": Events,
+    "ThisWeekInSTEM": ThisWeekInSTEM,
 }
 
 export const pagesConfig = {
